e8a14534011c368db17389cc1a09ce52946f0468
Tim Lardner <tim@lardner.io>
2015-12-14 16:31:53 +0000

    Fixed newline for suggest song link

--- steamiebot.py
+++ steamiebot.py
@@ -213,7 +213,7 @@ def createPost(r):
             + gigString + "\n\n"
             "**Tune of the day**\n\n"
             + tuneString
-            + "[Suggest tomorrow's tune](https://www.reddit.com/message/compose/?to=SteamieBot&amp;subject=SongRequest)")
+            + "\n\n[Suggest tomorrow's tune](https://www.reddit.com/message/compose/?to=SteamieBot&amp;subject=SongRequest)")
 
     return title,body
 
