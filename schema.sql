6417a3c4c0aaf403ec9198b1de35fca81fd6c44f
Tim Lardner <timlardner@gmail.com>
2016-05-23 15:01:28 +0100

    Fix schema

--- schema.sql
+++ schema.sql
@@ -1,20 +1,22 @@
+create table submitter (
+  user_id               integer primary key autoincrement not null,
+  age                   text,
+  name                  text
+);
+
 create table submitted (
     submitted_id        integer primary key autoincrement not null,
     name                text,
     url                 text,
     whensub             date,
     comment             text,
-    user_id             integer, FOREIGN KEY(username) references submitter(id)
+    user_id             integer, FOREIGN KEY(user_id) references submitter(user_id)
 );
 
 create table chosen (
     id                  integer primary key autoincrement not null,
-    submitted_id        integer, FOREIGN KEY(submitted_id) references submitted(id)
+    submitted_id        integer, FOREIGN KEY(submitted_id) references submitted(submitted_id)
 );
 
-create table submitter (
-  user_id               integer primary key autoincrement not null,
-  age                   text,
-  name                  text
-);
+
   
