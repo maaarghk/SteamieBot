8773d22f85f66b65b394c857f0f6f9ad03fd5a77
Tim Lardner <timlardner@gmail.com>
2016-05-23 13:29:21 +0100

    Fix dodgy line breaks

--- youtube.py
+++ youtube.py
@@ -15,8 +15,7 @@ class YouTubeInfo(object):
                 parser = argparse.ArgumentParser(parents=[argparser])
                 flags = argparser.parse_args()
                 if credentials is None or credentials.invalid:
-                        flow = 
-flow_from_clientsecrets("secrets/google_client_secrets.json",scope=scope)
+                        flow = flow_from_clientsecrets("secrets/google_client_secrets.json",scope=scope)
                         credentials = run_flow(flow, storage, flags)
                 yt = build("youtube", "v3", http=credentials.authorize(httplib2.Http()))
                 return yt
@@ -24,8 +23,7 @@ flow_from_clientsecrets("secrets/google_client_secrets.json",scope=scope)
         def getTitle(self,this_id):
                 if not self.validate(this_id):
                         return "Invalid ID"
-                response = 
-self.yt.videos().list(part="id,snippet,statistics,contentDetails",id=this_id).execute()
+                response = self.yt.videos().list(part="id,snippet,statistics,contentDetails",id=this_id).execute()
                 info = response['items'][0]
                 snippet = info['snippet']
                 title = snippet['title']
@@ -35,8 +33,7 @@ self.yt.videos().list(part="id,snippet,statistics,contentDetails",id=this_id).ex
                 self.yt = self.auth()
 
         def validate(self, this_id):
-                response = 
-self.yt.videos().list(part="id,snippet,statistics,contentDetails",id=this_id).execute()
+                response = self.yt.videos().list(part="id,snippet,statistics,contentDetails",id=this_id).execute()
                 if len(response['items']) != 1:
                         return False
                 else:
