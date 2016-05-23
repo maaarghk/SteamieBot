0c789592fe398184cf4a52d6ae3760f495c0a342
Tim Lardner <timlardner@gmail.com>
2016-05-23 14:52:26 +0100

    Updated schema for relational stuff

--- schema.sql
+++ schema.sql
@@ -1,18 +1,20 @@
 create table submitted (
-    id          integer primary key autoincrement not null,
-    name        text,
-    url         text,
-    whensub     date,
-    username    integer, FOREIGN KEY(username) references submitter(id)
+    submitted_id        integer primary key autoincrement not null,
+    name                text,
+    url                 text,
+    whensub             date,
+    comment             text,
+    user_id             integer, FOREIGN KEY(username) references submitter(id)
 );
 
 create table chosen (
-    id          integer primary key autoincrement not null,
-    submitted_id integer, FOREIGN KEY(submitted_id) references submitted(id)
+    id                  integer primary key autoincrement not null,
+    submitted_id        integer, FOREIGN KEY(submitted_id) references submitted(id)
 );
 
 create table submitter (
-  id integer primary key autoincrement not null,
-  age text,
-  name text
+  user_id               integer primary key autoincrement not null,
+  age                   text,
+  name                  text
 );
+  
