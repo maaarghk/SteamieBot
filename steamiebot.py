8ec6d6d72f93a7a857f0eb232fa96b4baaae9984
Tim Lardner <timlardner@gmail.com>
2016-05-25 10:46:04 +0100

    Regex fix

--- steamiebot.py
+++ steamiebot.py
@@ -55,7 +55,7 @@ def getMarket(r):
 def getSong(r): # Takes the PRAW object
     # Users must have held their account for this number of days to be able to submit suggestions
     how_old = 3
-    pattern = re.compile("(?:http[s]?://www\.youtube\.com/watch\?v=|http://youtu.be/)([0-9A-Za-z\-_]*)")
+    pattern = re.compile("(?:http[s]?://www\.youtube\.com/watch\?v=|http[s]?://youtu.be/)([0-9A-Za-z\-_]*)")
     song_strings = ["youtube.com/","youtu.be/"]
     messages = r.get_unread(unset_has_mail=True,update_user=True,limit=None)
 
