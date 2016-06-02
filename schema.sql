7520c493a950c03ccd75f481b784268d15a43e9f
Tim Lardner <timlardner@gmail.com>
2016-06-02 10:04:08 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 create table IF NOT EXISTS submitted (
     submitted_id        integer primary key autoincrement not null,
     url                 text,
-    whensub             text,
+    whensub             integer,
     username            text,
 );
 
 create table IF NOT EXISTS chosen (
-    chosen_date         text primary key,
+    chosen_date         integer primary key,
     url                 text,
     submitted_id        integer,
     FOREIGN KEY(submitted_id) references submitted(submitted_id)
