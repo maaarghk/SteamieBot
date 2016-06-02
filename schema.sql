bbfde3f1b11f14e4b01cc09f6488546c2734d0ff
Tim Lardner <timlardner@gmail.com>
2016-06-02 09:58:31 +0100

    Been reading a database book

--- schema.sql
+++ schema.sql
@@ -1,24 +1,15 @@
 create table IF NOT EXISTS submitted (
     submitted_id        integer primary key autoincrement not null,
-    title               text,
     url                 text,
     whensub             text,
     username            text,
-    UID                 text
 );
 
 create table IF NOT EXISTS chosen (
-    chosen_id           integer primary key autoincrement not null,
-    chosen_date         text,
+    chosen_date         text primary key,
     url                 text,
-    submitted_id        integer, FOREIGN KEY(submitted_id) references submitted(submitted_id)
+    submitted_id        integer,
+    FOREIGN KEY(submitted_id) references submitted(submitted_id)
 );
-
-create table IF NOT EXISTS ineligible (
-    ineligible_id       integer primary key autoincrement not null,
-    reason              text,
-    submitted_id        integer, FOREIGN KEY(submitted_id) references submitted(submitted_id)
-);
-
 
   
